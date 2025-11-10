c8cd5e913e677fca097dcdd5edd91a585525862f
Iaroslav Rykhalskyi <rikhalskiy@gmail.com>
2025-11-10 19:59:41 +0100

    Small redesign

--- frontend/src/app/collections-list/collections-list.component.ts
+++ frontend/src/app/collections-list/collections-list.component.ts
@@ -6,8 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { CollectionsService } from '../client/services/CollectionsService';
 import { Collection } from '../client/models/Collection';
 import { Router } from '@angular/router';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { AddCollectionDialogComponent } from '../add-collection-dialog/add-collection-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-collections-list',
@@ -19,7 +18,7 @@ import { AddCollectionDialogComponent } from '../add-collection-dialog/add-colle
 export class CollectionsListComponent implements OnInit {
   collections: Collection[] = [];
 
-  constructor(private router: Router, public dialog: MatDialog) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.fetchCollections();
@@ -37,29 +36,5 @@ export class CollectionsListComponent implements OnInit {
     this.router.navigate(['/collection', collectionId]);
   }
 
-  openAddCollectionDialog(): void {
-    const dialogRef = this.dialog.open(AddCollectionDialogComponent, {
-      width: 'auto',
-      height: 'auto',
-      data: {}
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        // Handle the result (new collection name) here
-        console.log('The dialog was closed with result:', result);
-        // Call API to create collection and refresh list
-        this.createCollection(result);
-      }
-    });
-  }
-
-  async createCollection(collectionName: string): Promise<void> {
-    try {
-      await CollectionsService.createNewCollectionCollectionsPost({ name: collectionName });
-      this.fetchCollections(); // Refresh the list
-    } catch (error) {
-      console.error('Error creating collection:', error);
-    }
-  }
+ 
 }
--- frontend/src/app/topbar/topbar.component.ts
+++ frontend/src/app/topbar/topbar.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CollectionsService } from '../client/services/CollectionsService';
+import { AddCollectionDialogComponent } from '../add-collection-dialog/add-collection-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, MatButtonModule, MatIconModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
+  
+  constructor(public dialog: MatDialog){}
+
+ openAddCollectionDialog(): void {
+    const dialogRef = this.dialog.open(AddCollectionDialogComponent, {
+      width: 'auto',
+      height: 'auto',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        // Handle the result (new collection name) here
+        console.log('The dialog was closed with result:', result);
+        // Call API to create collection and refresh list
+        this.createCollection(result);
+      }
+    });
+  }
+
+  async createCollection(collectionName: string): Promise<void> {
+    try {
+      await CollectionsService.createNewCollectionCollectionsPost({ name: collectionName });
+    } catch (error) {
+      console.error('Error creating collection:', error);
+    }
+  }
 
 }
